675755aaf1a07ab8c9e7d134d212cde584285bc6
nkhr <ricracn@gmail.com>
2021-07-11 12:37:30 +0900

    update

--- iaac/codes/flask_app/service/app.py
+++ iaac/codes/flask_app/service/app.py
@@ -14,12 +14,12 @@ conn = None
 DB_HOST = os.environ["DB_HOST"]
 DB_PORT = os.environ["DB_PORT"]
 DATABASE = os.environ["DATABASE"]
-DB_USER = os.environ["DB_USER"]
-DB_PASSWORD = os.environ["DB_PASSWORD"] 
+DB_USER = ""
+DB_PASSWORD = ""
 
 def get_secret():
     global rds_password
-    secret_name = "rds/password"
+    secret_id = "rds"
     region_name = "ap-northeast-1"
     # Create a Secrets Manager client
     session = boto3.session.Session()
@@ -29,7 +29,7 @@ def get_secret():
     )
     try:
         get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
+            SecretId=secret_id
         )
     except ClientError as e:
         if e.response['Error']['Code'] == 'DecryptionFailureException':
@@ -59,15 +59,15 @@ def get_secret():
             return base64.b64decode(get_secret_value_response['SecretBinary'])
 
 def set_connection():
-    rds_password = get_secret()
+    rds_secret = get_secret()
     try:
         global conn
         conn = psycopg2.connect(
             host=DB_HOST,
             port=DB_PORT,
             database=DATABASE,
-            user=DB_USER,
-            password=DB_PASSWORD
+            user=rds_secret.get('username'),
+            password=rds_secret.get('password')
         )
     except (Exception, psycopg2.DatabaseError) as err:
         print(set_connection.__name__, err)
@@ -86,7 +86,7 @@ def getResponse():
     response.headers["Content-Type"]= "application/json"
     return response
 
-@app.route("/db")
+@app.route("/get")
 def getResponse():
     try:
         global conn
@@ -106,6 +106,27 @@ def getResponse():
                 return test
     except:
         print('DB Error')
+
+@app.route("/create")
+def getResponse():
+    try:
+        global conn
+        with conn:
+            with conn.cursor() as curs:
+                curs.execute("""
+                    CREATE TABLE test (id INT, name VARCHAR(10));
+                    INSERT INTO test VALUES (1, 'test1'), (2, 'test2'), (3, 'test3');
+                """, (return_count,))
+
+                test = []
+                row = curs.fetchone()
+                while row is not None:
+                    test.append(row)
+                    row = curs.fetchone()
+                return test
+    except:
+        print('DB Error')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
